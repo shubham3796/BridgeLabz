589be0cd49e80f8d3531991122efc64c33e06f60
shubham <shubham5010175@gmail.com>
2019-09-03 10:57:19 +0530

    updated: Finding prime numbers less than 1000

--- week 1/algorithmProblems/2.swift
+++ week 1/algorithmProblems/2.swift
@@ -1,31 +1,24 @@
-//
-//  main.swift
-//  algo2
-//
-//  Created by admin on 22/08/19.
-//  Copyright © 2019 admin. All rights reserved.
-//
-
 import Foundation
 
 var arr = [Int]()
-var i:Int
+var divisor:Int
+var number:Int
 
-for i in 0...1000 {
-    arr.append(i)
+for number in 0...1000 {
+    arr.append(number)
 }
 var flag=0
 
 print("2")
 print("3")
-for i in 4...1000 {
+for number in 4...1000 {
     flag = 0
-    for j in 2...(i/2) {
-        if arr[i]%j == 0 {
+    for divisor in 2...(number/2) {
+        if arr[number]%divisor== 0 {//number is not prime, set flag as 1
             flag=1
         }
-    }
+    }//end for 
     if flag==0 {
-        print(i)
+        print(number)
     }
 }
