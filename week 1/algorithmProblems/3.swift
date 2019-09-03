a58799970fc1101329c422154b42307ceb0a5b32
shubham <shubham5010175@gmail.com>
2019-09-03 11:32:13 +0530

    updated:List of anagram primes & palindrome primes

--- week 1/algorithmProblems/3.swift
+++ week 1/algorithmProblems/3.swift
@@ -1,51 +1,48 @@
 
 import Foundation
 
-var arr = [Int]()
-var i:Int
-var oneDigitPrimes = [Int]()
+var array = [Int]()
+var number:Int
+var oneDigitPrimes = [Int]()//Array containing all one digit prime numbers
 var twoDigitPrimes = [Int]()
 var threeDigitPrimes = [Int]()
 var arrayA = [Int]()
 var arrayB = [Int]()
-
-for i in 0...1000 {
-    arr.append(i)
-}
 var flag=0
 
-
+//Check whether the two numbers represented as array of integers are anagram pairs or not
 func checkAnagram(_ arrayA:inout [Int],_ arrayB:inout [Int]) {
     //print("\(arrayA) \(arrayB)")
-    var lenA=arrayA.count
-    var lenB=arrayB.count
+    var lengthA=arrayA.count
+    var lengthB=arrayB.count
     var storeArrayA=arrayA
     var storeArrayB=arrayB
     
-    if lenA==lenB {
-        var i:Int
-        var j:Int
-        var temp2:Int
-        
-        for i in 0...(lenA-2) {
-            for j in i+1...(lenA-1){
-                if arrayA[j]<arrayA[i] {
-                    temp2=arrayA[j]
-                    arrayA[j]=arrayA[i]
-                    arrayA[i]=temp2
-                }
+    if lengthA==lengthB {
+    var iteratorA:Int
+    var iteratorB:Int
+    var temporaryVariable2:Int
+    
+ //Sorting array A
+    for iteratorA in 0...(lengthA-2) {
+        for iteratorB in iteratorA+1...(lengthA-1){
+            if stringA[iteratorB]<stringA[iteratorA] {//Swap the two
+                temporaryVariable2=stringA[iteratorB]
+                stringA[iteratorB]=stringA[iteratorA]
+                stringA[iteratorA]=temporaryVariable2
             }
         }
-        
-        for i in 0...(lenB-2) {
-            for j in i+1...(lenB-1){
-                if arrayB[j]<arrayB[i] {
-                    temp2=arrayB[j]
-                    arrayB[j]=arrayB[i]
-                    arrayB[i]=temp2
-                }
+    }
+    //Sorting array B 
+    for iteratorA in 0...(lengthB-2) {
+        for iteratorB in iteratorA+1...(lengthB-1){
+            if stringA[iteratorB]<stringA[iteratorA] {//Swap the two
+                temporaryVariable2=stringA[iteratorB]
+                stringA[iteratorB]=stringA[iteratorA]
+                stringA[iteratorA]=temporaryVariable2
             }
         }
+    }
         
        if arrayA==arrayB {
         print("\(storeArrayA)   \(storeArrayB)")
@@ -53,55 +50,30 @@ func checkAnagram(_ arrayA:inout [Int],_ arrayB:inout [Int]) {
     }
 }
 
-
-oneDigitPrimes.append(2)
-oneDigitPrimes.append(3)
-
-for i in 4...1000 {
-    flag = 0
-    for j in 2...(i/2) {
-        if arr[i]%j == 0 {
-            flag=1
-        }
-    }
-    if flag==0 {
-        if i/10 == 0 {
-            oneDigitPrimes.append(i)
-        }
-        else if i/100 == 0 {
-            twoDigitPrimes.append(i)
-        }
-        else {
-            threeDigitPrimes.append(i)
-        }
-    }
-}
-//print("\(twoDigitPrimes)")
-print("Anagram pairs are : ")
-
-//print(countTwoDigitPrimes)
+//Mkae distict pairs of primes and send to another method to test whether they are anagram pair or not.
 func sendTestPairs(_ twoDigitPrimes:inout [Int]) {
     var countTwoDigitPrimes=twoDigitPrimes.count
-    var k:Int
-    var tempA:Int
-    var tempB:Int
-    for i in 0...(countTwoDigitPrimes-2) {
-        //    var arrayA = [Int]()
-        //    var arrayB = [Int]()
-        tempA=twoDigitPrimes[i]
-        while(tempA>0) {
-            arrayA.append((tempA%10))
-            tempA=tempA/10
+    var twoDigitPrimeNumber:Int
+    
+    //Make an array storing the individual digits of the first two digit prime number.
+    for iterator in 0...(countTwoDigitPrimes-2) {  
+        twoDigitPrimeNumbertwoDigitPrimes[iterator]
+        while(twoDigitPrimeNumber>0) {
+            arrayA.append((twoDigitPrimeNumber%10))
+            twoDigitPrimeNumber=twoDigitPrimeNumber/10
         }
         arrayA.reverse()
-        for j in (i+1)...(countTwoDigitPrimes-1) {
-            tempB=twoDigitPrimes[j]
-            while(tempB>0) {
-                arrayB.append((tempB%10))
-                tempB=tempB/10
+        
+        //Make an array storing the individual digits of the second two digit prime number.
+        for iterator2 in (iterator+1)...(countTwoDigitPrimes-1) {
+            twoDigitPrimeNumber=twoDigitPrimes[iterator2]
+            while(twoDigitPrimeNumber>0) {
+                arrayB.append((twoDigitPrimeNumber%10))
+                twoDigitPrimeNumber=twoDigitPrimeNumber/10
             }
-            
             arrayB.reverse()
+            
+            
             checkAnagram(&arrayA , &arrayB)
             
             arrayB=[]
@@ -110,12 +82,42 @@ func sendTestPairs(_ twoDigitPrimes:inout [Int]) {
     }
 }
 
+
+for number in 0...1000 {
+    array.append(iterator)
+}
+
+oneDigitPrimes.append(2)
+oneDigitPrimes.append(3)
+
+for number in 4...1000 {
+    flag = 0
+    for divisor in 2...(number/2) {
+        if array[number]%divisor == 0 {
+            flag=1
+        }
+    }
+    if flag==0 {
+        if number/10 == 0 {
+            oneDigitPrimes.append(number)
+        }
+        else if number/100 == 0 {
+            twoDigitPrimes.append(number)
+        }
+        else {
+            threeDigitPrimes.append(number)
+        }
+    }
+}
+
+
+print("The anagram primes are:")
 sendTestPairs(&twoDigitPrimes)
 sendTestPairs(&threeDigitPrimes)
 
 print("The Palindrome prime numbers are:")
-for i in 0...(threeDigitPrimes.count-1) {
-    if threeDigitPrimes[i]/100 == threeDigitPrimes[i]%10 {
-        print(threeDigitPrimes[i])
+for iterator in 0...(threeDigitPrimes.count-1) {
+    if threeDigitPrimes[iterator]/100 == threeDigitPrimes[iterator]%10 {
+        print(threeDigitPrimes[iterator])
     }
 }
